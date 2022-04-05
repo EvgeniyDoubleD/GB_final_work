4f839167c07903c1ce4d0b5d0362843dd4751a48
Evgeniy <ugin2308@mail.ru>
2022-04-05 20:31:33 +0300

    added program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа решающая задачу. Массив решил задать изначально, чтобы не раздувать код и keep it simple соблюсти. :)
+
+string [] initArr = new string [] {"string1","Hi","and","thank","you", "for","fine","st","ud","y"};
+
+int c = 0;
+for (int i = 0; i < initArr.Length; i++)
+    { if (initArr[i].Length<=3)
+        c++;
+    }
+
+string [] newArr = new string [c];
+int k = 0;
+for (int j = 0; j < initArr.Length; j++)
+    { if (initArr[j].Length<=3)
+        {
+            newArr[k] = initArr[j];
+            Console.Write($"{newArr[k]}, ");
+            k++;
+        }
+    }
