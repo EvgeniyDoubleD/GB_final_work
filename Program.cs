30798e865482fc17f9d8deafccadfaa4ecf085e6
Evgeniy <ugin2308@mail.ru>
2022-04-05 20:38:08 +0300

    code culture fixes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿// Программа решающая задачу. Массив решил задать изначально, чтобы не раздувать код и keep it simple соблюсти. :)
 
-string [] initArr = new string [] {"string1","Hi","and","thank","you", "for","fine","st","ud","y"};
+string[] initArr = new string[] { "Teachers,", "Hi", "and", "thank", "you", "for", "awesome", "st", "ud", "y", ":)" };
 
 int c = 0;
 for (int i = 0; i < initArr.Length; i++)
-    { if (initArr[i].Length<=3)
+{
+    Console.Write($"[{initArr[i]}] ");
+    if (initArr[i].Length <= 3)
         c++;
-    }
+}
 
-string [] newArr = new string [c];
+Console.WriteLine();
+string[] newArr = new string[c];
 int k = 0;
 for (int j = 0; j < initArr.Length; j++)
-    { if (initArr[j].Length<=3)
-        {
-            newArr[k] = initArr[j];
-            Console.Write($"{newArr[k]}, ");
-            k++;
-        }
+{
+    if (initArr[j].Length <= 3)
+    {
+        newArr[k] = initArr[j];
+        Console.Write($"[{newArr[k]}] ");
+        k++;
     }
+}
